4f6f90bc0c7f0b61e8a010b45cf46e11db2ae5fa
pumecart <car21132@byui.edu>
2022-09-19 09:26:04 -0400

    almost there update

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,5 +1,14 @@
 const title = 'Title';
 document.getElementById('title').textContent = title;
+document.getElementById('title1').textContent = title;
+document.getElementById('title2').textContent = title;
+document.getElementById('title3').textContent = title;
+document.getElementById('title4').textContent = title;
+document.getElementById('title5').textContent = title;
+document.getElementById('title6').textContent = title;
+document.getElementById('title7').textContent = title;
+document.getElementById('title8').textContent = title;
+document.getElementById('title9').textContent = title;
 
 const currentyear = new Date().getFullYear();
 document.getElementById('year').textContent = currentyear;
