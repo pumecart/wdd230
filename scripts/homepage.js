733b6f5d5b8b5f27f3a9d93f8c9045e8a1b6cb86
pumecart <car21132@byui.edu>
2022-09-19 10:09:34 -0400

    updated with last update

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,20 +1,13 @@
-const title = 'Title';
-document.getElementById('title').textContent = title;
-document.getElementById('title1').textContent = title;
-document.getElementById('title2').textContent = title;
-document.getElementById('title3').textContent = title;
-document.getElementById('title4').textContent = title;
-document.getElementById('title5').textContent = title;
-document.getElementById('title6').textContent = title;
-document.getElementById('title7').textContent = title;
-document.getElementById('title8').textContent = title;
-document.getElementById('title9').textContent = title;
+
 
 const currentyear = new Date().getFullYear();
 document.getElementById('year').textContent = currentyear;
 
-const options = {month: 'numeric', day: 'numeric', year: 'numeric'};
-document.getElementById('lastupdated').textContent = `Last Updated: ${new Date().toLocaleDateString('en US', options)}`;
+// const options = {month: 'numeric', day: 'numeric', year: 'numeric'};
+// document.getElementById('lastupdated').innerHTML = `Last Updated: ${new Date().toLocaleDateString('en US', options)}`;
+
+let oLastModif = new Date(document.lastModified);
+document.getElementById('lastupdated').innerHTML = oLastModif;
 
 
 
