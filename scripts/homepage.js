4f213eb26b9780dcb0075e4207ea5e98f442c51d
pumecart <car21132@byui.edu>
2022-09-19 11:15:12 -0400

    final update

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -3,11 +3,10 @@
 const currentyear = new Date().getFullYear();
 document.getElementById('year').textContent = currentyear;
 
-// const options = {month: 'numeric', day: 'numeric', year: 'numeric'};
-// document.getElementById('lastupdated').innerHTML = `Last Updated: ${new Date().toLocaleDateString('en US', options)}`;
 
-let oLastModif = new Date(document.lastModified);
-document.getElementById('lastupdated').innerHTML = oLastModif;
+// let oLastModif = new Date(document.lastModified);
+// document.querySelector('#lastupdated').innerHTML = `Last Modified: ${oLastModif}`;
 
-
+const options = {month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+document.querySelector('#lastupdated').innerHTML = `Last Updated ${new Date().toLocaleDateString('en-US', options)}`;
 
