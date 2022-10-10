533b9256c823278843ad188c7b9edc482f287132
pumecart <car21132@byui.edu>
2022-10-10 12:24:04 -0400

    js update

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,16 +1,18 @@
-// const currentdate = new Date().getFullYear();
-// document.querySelector('.date').innerHTML = currentyear;
-
+// header today's date
 const datefield = document.querySelector(".date");
 
 const now = new Date();
-const date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
 
-datefield.innerHTML = `<em>${date}</em>`;
+datefield.innerHTML = `<em>${fulldate}</em>`;
 
+// footer- year
+const currentyear = new Date().getFullYear();
+document.getElementById('year').textContent = currentyear;
 
+// footer last updated
 const options = {month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
 document.querySelector('#lastupdated').innerHTML = `Last Updated ${new Date().toLocaleDateString('en-US', options)}`;
 
