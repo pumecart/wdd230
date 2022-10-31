a233a92fa4d8149afa1a37d726260c9330416bf2
pumecart <car21132@byui.edu>
2022-10-30 20:46:27 -0400

    discover initial

--- lesson-7/scripts/lazyload.js
+++ lesson-7/scripts/lazyload.js
@@ -30,3 +30,33 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach(image => {
     imgObserver.observe(image);
 })
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var lazyImages = document.querySelectorAll(".lazy");    
+//     var lazyTimeout;
+    
+//     function lazyload () {
+//       if(lazyTimeout) {
+//         clearTimeout(lazyTimeout);
+//       }    
+      
+//       lazyTimeout = setTimeout(function() {
+//           var scroll = window.pageYOffset;
+//           lazyImages.forEach(function(img) {
+//               if(img.offsetTop < (window.innerHeight + scroll)) {
+//                 img.src = img.dataset.src;
+//                 img.classList.remove('lazy');
+//               }
+//           });
+//           if(lazyImages.length == 0) { 
+//             document.removeEventListener("scroll", lazyload);
+//             window.removeEventListener("resize", lazyload);
+//             window.removeEventListener("orientationChange", lazyload);
+//           }
+//       }, 20);
+//     }
+    
+//     document.addEventListener("scroll", lazyload);
+//     window.addEventListener("resize", lazyload);
+//     window.addEventListener("orientationChange", lazyload);
+//   });
