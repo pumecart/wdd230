811b2c318e681645e06a975c692e341c54610936
pumecart <car21132@byui.edu>
2022-11-11 14:44:41 -0500

    prophet update

--- lesson-9/scripts/prophets.js
+++ lesson-9/scripts/prophets.js
@@ -2,27 +2,30 @@ const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-
 const cards = document.querySelector('.cards');
 
 async function getProphets() {
-    const response = await fectch(requestURL);
+    const response = await fetch(requestURL);
     if(response.ok) {
-        const data = await response.json(jsonObject);
-        // return data.prophets;
-        const prophets = jsonObject['prophets'];
-        return prophets;
+        const data = await response.json();
+        data.prophets.forEach(prophet => {displayProphets(prophet)} )
     }
 }
 
-
 function displayProphets(prophet) {
-
-    prophets.forEach(prophet => {
+    // prophets.forEach(prophet => {
 
         // Create elements to add to the document
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let portrait = document.createElement('img');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('div');
+        
+
     
         // Change the textContent property of the h2 element to contain the prophet's full name
         h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
     
         // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
         portrait.setAttribute('src', prophet.imageurl);
@@ -35,12 +38,14 @@ function displayProphets(prophet) {
         // Add/append the section(card) with the h2 element
         card.appendChild(h2);
         card.appendChild(portrait);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
     
         // Add/append the existing HTML div with the cards class with the section(card)
         // document.querySelector('div.cards').appendChild(card);
         cards.appendChild(card);
-    });
+    // });
 }
 
-const prophets = getProphets();
-displayProphets(data.prophets);
+getProphets();
+// displayProphets(data);
