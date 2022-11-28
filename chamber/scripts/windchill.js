aeda9d7d373f98f76a3c009f94f9f24ff0089742
pumecart <car21132@byui.edu>
2022-11-27 21:53:29 -0500

    discover updates

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -2,20 +2,20 @@
 const currentTemp = document.querySelector('#current-temp');
 const description = document.querySelector('#currently');
 const weatherIcon = document.querySelector('#weather-icon');
-const caption = document.querySelector('#weatherfig');
+const caption = document.querySelector('weatherfig');
 const windSpeed = document.querySelector('#windspeed');
 const windChill = document.querySelector('#windchill');
 
 
 const apiURL = 
-    'https://api.openweathermap.org/data/2.5/weather?q=East+Hartford&units=imperial&appid=81f8043dc05be8824a14c93c95981048';
+    'https://api.openweathermap.org/data/2.5/weather?q=Hartford&units=imperial&appid=81f8043dc05be8824a14c93c95981048';
 
 async function apiFetch() {
     try {
       const response = await fetch(apiURL);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // this is for testing the call
+        console.log(data);
         displayResults(data);
       } else {
           console.log(`Response not OK ${await response.text()}`);
@@ -32,25 +32,25 @@ async function apiFetch() {
   }
 
   function displayResults(data) {
-    let tempe = `<strong>${data.main.temp.toFixed(0)}</strong>`;
-    currentTemp.innerHTML = tempe;
-    let desc = capitalize(data.weather[0].description);
+    let ctemp = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+    currentTemp.innerHTML = ctemp;
+    let desc = `${capitalize(data.weather[0].description)}`;
     description.textContent = desc;
     let iconCap = `Icon of current weather condition at Wickham Park which is ${desc}`;
-    caption.textContent = iconCap;
+    caption.innerHTML = iconCap;
     weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     weatherIcon.alt = iconCap;
     let windsp = `${data.wind.speed} MPH`;
     windSpeed.textContent = windsp;
   }
     
-    let calc= Math.round((35.74 + (0.6215 * tempe))-(35.75 * Math.pow(windsp,0.16)) + (0.4275*tempe*Math.pow(windsp,0.16)));
-    let windchill = "";
-    if (tempe <= 50 && windsp > 3) {
-        windchill = (t, s);
-          calc = `${calc}&#176;F`;
-        } else {
-          calc = "N/A";
-        }
+    // let calc= Math.round((35.74 + (0.6215 * currentTemp))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*currentTemp*Math.pow(windSpeed,0.16)));
+    // let windchill = "";
+    // if (currentTemp <= 50 && windSpeed > 3) {
+    //     windchill = (t, s);
+    //       calc = `${calc}&#176;F`;
+    //     } else {
+    //       calc = "N/A";
+    //     }
 
-    windChill.textContent = calc;
+    // windChill.textContent = calc;
