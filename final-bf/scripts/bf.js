0c3659947591f1d6ded2d7c448fd93f5cee7932d
pumecart <car21132@byui.edu>
2022-12-14 18:35:38 -0500

    basecurrenttemp updates

--- final-bf/scripts/bf.js
+++ final-bf/scripts/bf.js
@@ -12,3 +12,43 @@ x.onclick = toggleMenu;
 const options = {month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
 document.querySelector('#lastupdated').innerHTML = `Last Updated ${new Date().toLocaleDateString('en-US', options)}`;
 
+//current weather
+const currentTemp = document.querySelector('#current-temp');
+const description = document.querySelector('#currently');
+const weatherIcon = document.querySelector('#weather-icon');
+const humidity = document.querySelector('#humidity');
+
+const apiURL = 
+    'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=81f8043dc05be8824a14c93c95981048';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(apiURL);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          console.log(`Response not OK ${await response.text()}`);
+      }
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
+  }
+
+  apiFetch();
+
+  function capitalize(string) {
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+  }
+
+  function displayResults(data) {
+    currentTemp.innerHTML = `<strong>${data.main.temp.toFixed(0)}</strong>`;
+    let desc = capitalize(data.weather[0].description);
+    description.textContent = desc;
+    humidity.innerHTML = `${data.main.humidity}`;
+    weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  }
+  
+
+
