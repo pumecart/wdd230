f03540f6c2693056be9481cf9444c1bcd3f1c39c
pumecart <car21132@byui.edu>
2022-12-14 12:18:59 -0500

    footer updates

--- final-bf/scripts/bf.js
+++ final-bf/scripts/bf.js
@@ -1,9 +1,14 @@
+// hamburgerBtn
 function toggleMenu () {
     document.getElementById("navigation").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
-    //add if statement to make it change to an x and then change it back
 };
 
 const x = document.getElementById('hamburgerBtn');
 
 x.onclick = toggleMenu;
+
+// footer last updated
+const options = {month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+document.querySelector('#lastupdated').innerHTML = `Last Updated ${new Date().toLocaleDateString('en-US', options)}`;
+
